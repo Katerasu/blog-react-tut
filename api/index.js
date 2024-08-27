ef8be298b73f17506969998d08fd9be2b961ed0a
Katerasu <dkthong2010@gmail.com>
2024-08-27 23:10:11 +0700

    load posts from db to page

--- api/index.js
+++ api/index.js
@@ -17,7 +17,8 @@ const secret = 'conmeo123';
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'))
 
 mongoose.connect('mongodb://localhost:27017/react-tut-blog')
     .then((result) => console.log('Connected to mongodb'))
@@ -80,14 +81,31 @@ app.post('/post', uploadMW.single('file'), async (req, res) => {
     const newPath = path+'.'+ext
     fs.renameSync(path, newPath);
 
-    const {title, summary, content} = req.body;
-    const postDoc = await PostModel.create({
-        title,
-        summary,
-        content,
-        cover: newPath,
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const {title, summary, content} = req.body;
+        const postDoc = await PostModel.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
+        })
+        res.json(postDoc);
     })
-    res.json(postDoc);
+
+
+    
 });
+
+app.get('/post', async (req, res) => { 
+    res.json(
+        await PostModel.find()
+            .populate('author', ['username'])
+            .sort({createdAt: -1})
+            .limit(20)
+    );
+})
 
 app.listen(4000);
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,18 @@
-export default function Post() {
+import { formatISO9075 } from "date-fns";
+
+export default function Post({title, summary, cover, content, createdAt, author}) {
     return (
         <div className="post">
           <div className="image">
-            <img src="https://duet-cdn.vox-cdn.com/thumbor/0x0:1280x720/750x750/filters:focal(640x360:641x361):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/25585694/sc8i9c.jpg" alt="" />
+            <img src={"http://localhost:4000/" + cover} alt="" />
           </div>
           <div className="texts">
-            <h2>Black Myth: Wukong is too mediocre for all this drama</h2>
+            <h2>{title}</h2>
             <p className="info">
-              <a href="/" className="author">kdam</a>
-              <time dateTime="">2024-08-24 23:23</time>
+              <a href="/" className="author">{author.username}</a>
+              <time dateTime="">{formatISO9075(new Date(createdAt))}</time>
             </p>
-            <p className="summary">Black Myth: Wukong is a hit: breaking all-time peak player count records on Steam, beating out Counter-Strike and Palworld to become the second most-played game on the platform, and selling more than 10 million copies across all platforms. In the few days after its release, it has become one of the most talked-about games of the summer — but for all the wrong reasons. </p>
+            <p className="summary">{summary}</p>
           </div>
         </div>
     );
